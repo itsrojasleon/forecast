7a9f40525c55f0fb48edd380b4543be30bcf1675
Juan Luis Rojas Leon <rojasleonjuan122@gmail.com>
2018-04-23 16:00:50 -0500

    Labeling units

--- src/components/chart.js
+++ src/components/chart.js
@@ -1,12 +1,19 @@
 import React from 'react';
-import { Sparklines, SparklinesLine } from 'react-sparklines';
+import _ from 'lodash';
+import { Sparklines, SparklinesLine, SparklinesReferenceLine } from 'react-sparklines';
 
-function Chart({ temps, color }) {
+function average(data) {
+  return _.round(_.sum(data) / data.length);
+}
+
+function Chart({ temps, color, units }) {
   return (
     <div>
       <Sparklines height={120} width={180} data={temps}>
         <SparklinesLine color={color} />
+        <SparklinesReferenceLine type="avg" />
       </Sparklines>
+      <div>{average(temps)} {units}</div>
     </div>
   );
 }
--- src/containers/weather-list.js
+++ src/containers/weather-list.js
@@ -1,18 +1,19 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import randomColor from 'randomcolor';
 import Chart from '../components/chart';
 
 class WeatherList extends Component {
   renderWeather = (cityData) => {
     const { name } = cityData.city;
     const temps = cityData.list.map(weather => weather.main.temp);
-    const color = randomColor();
-    console.log(temps);
+    const pressures = cityData.list.map(weather => weather.main.pressure);
+    const humidities = cityData.list.map(weather => weather.main.humidity);
     return (
       <div key={name}>
         <span>
-          <Chart temps={temps} color={color} />
+          <Chart temps={temps} color="orange" units="K" />
+          <Chart temps={pressures} color="green" units="hPa" />
+          <Chart temps={humidities} color="purple" units="%" />
         </span>
       </div>
     );
@@ -22,9 +23,9 @@ class WeatherList extends Component {
       <div>
         <div>
           <span>City</span>
-          <span>Temperature</span>
-          <span>Pressure</span>
-          <span>Humidity</span>
+          <span>Temperature (K)</span>
+          <span>Pressure (hPa)</span>
+          <span>Humidity (%)</span>
         </div>
         <div>
           {this.props.weather.map(this.renderWeather)}
