f17391541fd72f73267e7cb117b30afe1cb057c8
Juan Luis Rojas Leon <rojasleonjuan122@gmail.com>
2018-04-22 21:21:24 -0500

    Controlled Components

--- src/containers/search-bar.js
+++ src/containers/search-bar.js
@@ -1,18 +1,29 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component {
+  state = {
+    term: '',
+  };
+
+  onInputChange = (e) => {
+    const term = e.target.value;
+    this.setState(() => ({ term }));
+  }
+
   render() {
     return (
       <form className="form">
         <input
+          onChange={this.onInputChange}
           className="input-search"
           type="text"
-          placeholder="City"
+          placeholder="Get a five-day forecast in your favorite cities."
+          value={this.state.term}
         />
         <button
           type="submit"
-          className="button-submit"
-        >Submit
+          className="button-submit">
+          Submit
         </button>
       </form>
     );
