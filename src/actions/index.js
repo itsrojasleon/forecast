f92dcd75dac74bea6ab1b3d18178d35e6b099c74
Juan Luis Rojas Leon <rojasleonjuan122@gmail.com>
2018-04-22 23:08:42 -0500

    API working

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,13 +1,13 @@
 import 'whatwg-fetch';
 
-const API_KEY = 'b6907d289e10d714a6e88b30761fae22';
-const BASE_URL = `http://samples.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+const API_KEY = 'b714ec74bbab5650795063cb0fdf5fbe';
+const BASE_URL = 'https://api.openweathermap.org/data/2.5';
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
 export const fetchWeather = async (city) => {
-  const url = `${BASE_URL}&q=${city},us`;
-  const request = await fetch(url);
+  const response = await fetch(`${BASE_URL}/forecast?&q=${city}&type=accurate&appid=${API_KEY}&cnt=5`);
+  const request = await response.json();
   return {
     type: FETCH_WEATHER,
     payload: request,
