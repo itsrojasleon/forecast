15b718855c894d759281d3cd3436f8b8fb98ccf5
Juan Luis Rojas Leon <rojasleonjuan122@gmail.com>
2018-04-23 15:29:37 -0500

    Adding Sparklines component

--- src/containers/weather-list.js
+++ src/containers/weather-list.js
@@ -1,15 +1,23 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Sparklines, SparklinesLine } from 'react-sparklines';
 
 class WeatherList extends Component {
   renderWeather = (cityData) => {
     const { name } = cityData.city;
+    const temps = cityData.list.map(weather => weather.main.temp);
+    console.log(temps);
     return (
-      <div key={name}>{name}</div>
+      <div key={name}>
+        <span>
+          <Sparklines height={120} width={180} data={temps}>
+            <SparklinesLine color="red" />
+          </Sparklines>
+        </span>
+      </div>
     );
   }
   render() {
-    console.log(this.props.weather);
     return (
       <div>
         <div>
@@ -19,6 +27,7 @@ class WeatherList extends Component {
           <span>Humidity</span>
         </div>
         <div>
+          {this.props.weather.map(this.renderWeather)}
         </div>
       </div>
     );
--- src/reducers/weather.js
+++ src/reducers/weather.js
@@ -3,7 +3,7 @@ import { FETCH_WEATHER } from '../actions/index';
 export default function weatherReducer(state = [], action) {
   switch (action.type) {
     case FETCH_WEATHER:
-      return [action.payload.data, ...state];
+      return state.concat([action.payload]);
     default:
       return state;
   }
