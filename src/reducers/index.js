ddb3b882023c7f197b6993a2b05d125564aec199
Juan Luis Rojas Leon <rojasleonjuan122@gmail.com>
2018-04-23 23:53:28 -0500

    Here's a problem with loading reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
+import { loadingBarReducer } from 'react-redux-loading';
 import weatherReducer from './weather';
 
 export default combineReducers({
   weather: weatherReducer,
+  loading: loadingBarReducer,
 });
